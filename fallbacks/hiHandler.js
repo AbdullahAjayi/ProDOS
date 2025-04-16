48087ef4fb61f3336c85c026d2ed2e1b67145fd5
AbdullahAjayi <hafizajayi2308@gmail.com>
2025-04-16 08:58:26 +0100

    added a folder to handle fallback messages

---
+++ fallbacks/hiHandler.js
@@ -0,0 +1,27 @@
+const WELCOME_MSG =
+  "Hi there! I'm ProDOS, your WhatsApp productivity partner.\nHow can I assist you today? You can ask me to log your habits, set reminders, or check your progress. Just type 'help' for a list of commands."
+
+/* for .help:
+    *Here are a list of commands you can use to get started:
+    .log — log a new habit  
+    .reminder — set or update habit reminders  
+    .list habits — see your habits  
+    .delete — delete a habit  
+  */
+
+module.exports = async (client, message) => {
+  const saidHi = message.body.match(/^(hi|hello)( prodos)?$/i)
+
+  try {
+    if (saidHi) {
+      await message.reply(WELCOME_MSG)
+      return true
+    }
+    return false
+  } catch (error) {
+    console.error("Error sending welcome message:", error)
+    message.reply(
+      "❌ An error occurred while processing your message. Please try again.later "
+    )
+  }
+}
