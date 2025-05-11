ad295be4f9c042764cfe74eae482a6a59c429f8a
AbdullahAjayi <hafizajayi2308@gmail.com>
2025-05-11 20:51:00 +0100

    feature: added confirmation step in delete command

--- commands/delete.js
+++ commands/delete.js
@@ -1,8 +1,11 @@
 const safeReply = require("../handlers/safeReply")
 const Habit = require("../models/habit")
+const userStates = require("../state/session")
 
 module.exports = async (client, message) => {
+  const userId = message.from
   const input = message.body.trim().toLowerCase()
+  const userState = userStates[userId] || {}
 
   const parts = input
     .split(" ")
@@ -12,31 +15,84 @@ module.exports = async (client, message) => {
 
   const habitName = parts.join(" ")
 
-  if (!habitName) {
+  if (!habitName && !userState.step) {
     return safeReply(
       client,
       message,
       "*Please enter a habit name to delete*\nExample: delete exercise"
     )
   }
-  const habit = await Habit.findOne({ userId: message.from, name: habitName })
 
-  if (!habit) {
-    return safeReply(
+  console.log("Received message:", input)
+  console.log("Current user state:", userState)
+
+  try {
+    if (!userState.step) {
+      const habit = await Habit.findOne({ userId: message.from, name: habitName })
+
+      if (!habit) {
+        return safeReply(
+          client,
+          message,
+          `*${habitName}* not found in your list of habits.\nEnsure you're typing the habit name correctly.\n\nUse the *list* commmand to see your habits`
+        )
+      }
+
+      //   Set context at the beginning of the flow
+      userState.context = "delete"
+      userState.step = "confirmation"
+      userState.habitName = habitName
+      userStates[userId] = userState
+
+      return safeReply(
+        client,
+        message,
+        "Are you sure you want to delete this habit?\nYes/No"
+      )
+    }
+
+    if (userState.step === "confirmation") {
+      if (input === "yes") {
+        const { habitName } = userState
+        await Habit.deleteOne({ userId: message.from, name: habitName })
+
+        // delete user state
+        delete userStates[userId]
+
+        console.log("Habit deleted successfully")
+
+        // successs message
+        return safeReply(
+          client,
+          message,
+          `*${
+            habitName.charAt(0).toUpperCase() + habitName.slice(1)
+          }* has been successfully deleted from your list of habits.`
+        )
+      } else if (input === "no") {
+        delete userStates[userId]
+        console.log("Terminated habit deletion process")
+        return safeReply(
+          client,
+          message,
+          "Habit deletion process terminated successfully."
+        )
+      } else {
+        console.log("Invalid input", input)
+        return safeReply(
+          client,
+          message,
+          "Please respond with 'Yes' or 'No' to confirm or cancel the habit deletion."
+        )
+      }
+    }
+  } catch (error) {
+    console.log("Error deleting habit", error)
+    delete userStates[userId]
+    safeReply(
       client,
       message,
-      `*${habitName}* not found in your list of habits.\nEnsure you're typing the habit name correctly.\n\nUse the *list* commmand to see your habits`
-    )
-  }
-
-  if (habit) {
-    await Habit.deleteOne({ userId: message.from, name: habitName })
-    return safeReply(
-      client,
-      message,
-      `*${
-        habit.name.charAt(0).toUpperCase() + habit.name.slice(1)
-      }* has been successfully deleted from your list of habits.`
+      "❌ An error occured while deleting your habit. Please try again"
     )
   }
 }
