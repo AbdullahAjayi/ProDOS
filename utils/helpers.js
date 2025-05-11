f1bdffb992e5ff7dc971b088462d0ffa6a8795bd
AbdullahAjayi <hafizajayi2308@gmail.com>
2025-05-11 12:16:33 +0100

    added new helper: addCorrectSuffix for user date input

--- utils/helpers.js
+++ utils/helpers.js
@@ -20,6 +20,30 @@ const formatDate = (date, locale = "en-US") => {
   })
 }
 
+function addCorrectSuffix(date) {
+  const num = parseInt(date, 10) // Convert the date to a number
+  if (isNaN(num)) return date // If it's not a valid number, return as is
+
+  const lastDigit = num % 10
+  const lastTwoDigits = num % 100
+
+  // Determine the correct suffix
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13) {
+    return `${num}th`
+  }
+  if (lastDigit === 1) {
+    return `${num}st`
+  }
+  if (lastDigit === 2) {
+    return `${num}nd`
+  }
+  if (lastDigit === 3) {
+    return `${num}rd`
+  }
+  return `${num}th`
+}
+
 module.exports = {
   formatDate,
+  addCorrectSuffix,
 }
