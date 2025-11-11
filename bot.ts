0857ca09a01dd6447ba9fe1503b02e22fb6719f1
Abdullah Ajayi <hafizajayi2308@gmail.com>
2025-11-11 18:33:41 +0100

    added habit creation conversation flow to bot.ts

--- bot.ts
+++ bot.ts
@@ -2,8 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { Bot, Context } from 'grammy';
-import { type ConversationFlavor, conversations } from "@grammyjs/conversations";
+import { type ConversationFlavor, conversations, createConversation } from "@grammyjs/conversations";
 import { registerOnboarding } from "./onboarding";
+import createHabit from "./logic/habit/createHabit";
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 if (!BOT_TOKEN) {
@@ -20,7 +21,7 @@ async function setBotCommands() {
   await bot.api.setMyCommands([
     { command: "start", description: "Start a conversation with me" },
     { command: "help", description: "Show help message" },
-    { command: "create_habit", description: "Create an habit ex: create_habit reading" },
+    { command: "create_habit", description: "Create a new habit" },
   ]);
 }
 setBotCommands().catch(err => console.log(err))
@@ -28,12 +29,18 @@ setBotCommands().catch(err => console.log(err))
 // Register the start command
 registerOnboarding(bot);
 
+// Create habit command
+bot.use(createConversation(createHabit));
+bot.command('create_habit', async (ctx) => await ctx.conversation.enter("createHabit"))
+
 // Other text from chat
 bot.on("message:text", (ctx) => {
   ctx.reply(`You said: <b><i>${ctx.msg.text}</i></b>`, {
     parse_mode: 'HTML'
   });
 });
+
+
 
 console.log(".\n.\n.\nProDOS Telegram Bot is running...");
 bot.start()
